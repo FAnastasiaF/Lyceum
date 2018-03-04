ea35fea0a20152ab4d23460758832a824037c4a0
FAnastasiaF <35655602+FAnastasiaF@users.noreply.github.com>
2018-03-04 23:06:39 +0300

    Add files via upload

--- new_file.py
+++ new_file.py
@@ -1,8 +1,50 @@
 import vk
+import time
 session = vk.Session()
-vk_api = vk.API(session, v='5.35')
-vk_api.users.get(user_id=1)
-session = vk.AuthSession('users_id', 'login', 'pass', scope='wall, messages, friends')
-vk_api = vk.API(session, v='5.35')
-vk_api.friends.getOnline(user_id=id, list_id = 1, online_mobile = 1)
-
+vk_api = vk.API(session, v = '5.35')
+session = vk.AuthSession('app_id', 'login', 'password', scope='wall, messages, friends')
+vk_api = vk.API(session, v = '5.35')
+f = vk_api.friends.getOnline(user_id = id, list_id = 1, online_mobile = 1)
+vk_api = vk.API(session, v = '5.35')
+b = f['online']
+c = f['online_mobile']
+a = b
+a.extend(c)
+i = len(a)
+while i!=0:
+r = vk_api.users.get(user_id = a[i-1])
+h = r[0]
+d = h['first_name']
+e = h['last_name']
+print(d, e)
+i = i-1
+time.sleep(1/3)
+q = len(a)
+while q!=0:
+print ()
+r = vk_api.users.get(user_id = a[q-1])
+h = r[0]
+d = h['first_name']
+p = h['last_name']
+print(d, p, ":")
+print ()
+session = vk.Session()
+vk_api = vk.API(session, v = '5.35')
+session = vk.AuthSession('app_id', 'login', 'password', scope='wall, messages, friends')
+vk_api = vk.API(session, v = '5.35')
+n = vk_api.friends.getOnline(user_id = a[q-1], list_id = 1, online_mobile = 1)
+vk_api = vk.API(session, v = '5.35')
+b = n['online']
+c = n['online_mobile']
+e = b
+e.extend(c)
+i = len(e)
+while i!=0:
+r = vk_api.users.get(user_id = e[i-1])
+h = r[0]
+d = h['first_name']
+p = h['last_name']
+print(d, p)
+i = i-1
+time.sleep(1/3)
+q = q-1
